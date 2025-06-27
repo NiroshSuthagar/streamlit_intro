ff82d7b6f4938476499f3b5ba55703a110a5022d
NiroshSuthagar <44868920+NiroshSuthagar@users.noreply.github.com>
2025-06-27 11:08:24 +0100

    Added csv and app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import streamlit as st
 import pandas as pd
+from sklearn import datasets
 
-df = pd.DataFrame({
-    'name': ['Nirosh', 'Sam', 'Jane'],
-    'scores': [36,45,54]
-})
-
+df = pd.read_csv('cdc.csv')
 st.write("Here's my first attempt at using streamlit and Pandas")
 st.write(df)
+
+df_plot = df.loc[df.gender == 'm',['height', 'weight']]
+
+st.scatter_chart(
+    df_plot
+)
+
+
+
